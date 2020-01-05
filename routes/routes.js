8ccca44f2c67c80a1c19fc651e39b4bfec7ffe9b
Uche Ozoemena <ucheozoemena@gmail.com>
2020-01-05 17:15:34 +0100

    Create whitelist of allowed origins for CORS

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,23 @@
+const cors = require('cors');
+
+const corsWhitelist = [
+  'http://localhost:8000',
+];
+const corsConfig = {
+  origin: (origin, callback) => {
+    if (corsWhitelist.indexOf(origin) !== -1) {
+      // this origin is allowed
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS!'));
+    }
+  },
+};
+
 const appRouter = app => {
+  // applies to all routes
+  app.use(cors(corsConfig));
+
   app.get('/', (req, res) => {
     res.status(200).send('Welcome to Coin Tracker API!');
   });
