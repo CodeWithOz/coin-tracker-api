dd5cf95f42570c16d55c33660872c5abb4ce2307
Uche Ozoemena <ucheozoemena@gmail.com>
2020-01-05 17:53:16 +0100

    Create module for fetching data from CoinMarketCap

---
+++ requests/requests.js
@@ -0,0 +1,29 @@
+const rp = require('request-promise');
+const { COIN_MARKET_CAP_API_KEY } = require('../secrets');
+
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD',
+  },
+  headers: {
+    'X-CMC-PRO_API_KEY': COIN_MARKET_CAP_API_KEY,
+  },
+  json: true,
+  gzip: true,
+};
+
+module.exports = {
+  getCMCData: cb => {
+    rp(requestOptions).then(response => {
+      if (typeof cb === 'function') {
+        cb(response);
+      }
+    }).catch(err => {
+      console.log('Something went wrong:', err);
+    });
+  },
+};
