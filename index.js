12cb9959610fc38b6ce336ad1addfa50b9eeac12
Uche Ozoemena <ucheozoemena@gmail.com>
2020-01-05 16:51:27 +0100

    Create basic app config

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /**
  * Required External Modules
  */
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
 
 /**
  * App Variables
@@ -9,11 +13,17 @@
 /**
  *  App Configuration
  */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
  * Routes Definitions
  */
+routes(app);
 
 /**
  * Server Activation
  */
+const server = app.listen(3000, function() {
+  console.log(`app running on port ${server.address().port}`);
+});
