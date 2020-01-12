cb2472c769a8be2337c5719eedd2a23ee42991d5
Uche Ozoemena <ucheozoemena@gmail.com>
2020-01-12 18:36:45 +0100

    Pass fetch error response to supplied callback
    This ensures that the server responds to the client with
    the CMC error.

--- requests/requests.js
+++ requests/requests.js
@@ -5,9 +5,9 @@ const requestOptions = {
   method: 'GET',
   uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
   qs: {
-    'start': '1',
-    'limit': '50',
-    'convert': 'USD',
+    start: '1',
+    limit: '50',
+    convert: 'USD',
   },
   headers: {
     'X-CMC_PRO_API_KEY': COIN_MARKET_CAP_API_KEY,
@@ -18,13 +18,16 @@ const requestOptions = {
 
 module.exports = {
   getCMCData: cb => {
-    rp(requestOptions).then(response => {
-      console.log('Response succeeded.');
-      if (typeof cb === 'function') {
-        cb(response);
-      }
-    }).catch(err => {
-      console.log('Something went wrong:', err.error.status.error_message);
-    });
+    rp(requestOptions)
+      .then(response => {
+        console.log('Response succeeded.');
+        if (typeof cb === 'function') {
+          cb(response);
+        }
+      })
+      .catch(err => {
+        console.log('Something went wrong:', err.error.status.error_message);
+        cb(err);
+      });
   },
 };
